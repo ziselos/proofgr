e9fee41684b5a20a2eee3a55b09d14813bd5f45b
epidrome <github@epidro.me>
2017-12-30 17:50:34 +0200

    weasyprint pipeline

---
+++ manuscript/md2html2pdf.sh
@@ -0,0 +1,3 @@
+cat yaml.md pi-intro.md pi-ch01.md pi-ch02.md pi-ch03.md pi-ch04.md pi-ch05.md pi-ch06.md > chapters.md
+pandoc -s --toc -c style.css chapters.md -o chapters.html
+weasyprint chapters.html chapters.pdf
